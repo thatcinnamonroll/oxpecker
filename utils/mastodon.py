844be737800b8b2f7e6f78c9149ff1d9bf984d21
Mateusz <thatcinnamonrolldev@proton.me>
2026-02-26 20:40:52 +0100

    added sending img to mastodon

--- utils/mastodon.py
+++ utils/mastodon.py
@@ -5,7 +5,26 @@ class mastodonBot:
         self.botToken = botToken
         self.botUrl = f"https://{botUrl}"
 
-    def post(self,newPost):
+        # media in post func means list/array with open("path/to/file",'rb')
+    def post(self,newPost,media=None):
         requestData = {'status':newPost}
+        if media is not None:
+            mediaList = []
+            for mediaFile in media:
+                mediaId = self.sendMedia(mediaFile)
+                mediaList.append(mediaId)
+            requestData['media_ids[]'] = mediaList
         requestHeader = {'Authorization': f'Bearer {self.botToken}'}
         requests.post(f"{self.botUrl}/api/v1/statuses",data=requestData,headers=requestHeader)
+
+    def sendMedia(self,media):
+        requestHeader = {'Authorization': f'Bearer {self.botToken}'}
+        requestMedia = {'file':media}
+        request = requests.post(f"{self.botUrl}/api/v1/media",files = requestMedia,headers=requestHeader)
+        response = request.json()
+        if request.status_code == 200 or request.status_code == 202:
+            imgId = str(response.get("id"))
+            print("media sent")
+        else:
+            imgId = None
+        return imgId
